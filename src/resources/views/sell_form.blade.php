a4cf431346eb8abadf9cb4355ce62b48a1b202e5
S <serena.sa.em.na.ra@gmail.com>
2025-10-13 21:24:00 +0900

    商品出品画面のview・css

--- src/resources/views/sell_form.blade.php
+++ src/resources/views/sell_form.blade.php
@@ -12,39 +12,57 @@
         @csrf
 
         <div class="profile-edit-image-area">
-
             <label for="name" class="sell-form-label">商品画像</label>
-            <input id="name" type="text" class="sell-form-input-1 @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+            <div class="input-wrapper">
+                <input id="name" type="text" class="sell-form-input-1 @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+
+                <label for="image" class="profile-edit-custom-file">画像を選択する</label>
+                <input id="image" type="file" class="profile-edit-input__file @error('image') is-invalid @enderror" name="image" accept="image/*">
+            </div>
+
             @error('name')
                 <span class="invalid-feedback" role="alert">
                     <strong>{{ $message }}</strong>
                 </span>
             @enderror
-
-            <label for="image" class="profile-edit-custom-file">
-                画像を選択する
-            </label>
-            <input id="image" type="file" class="profile-edit-input__file @error('image') is-invalid @enderror" name="image" accept="image/*">
             @error('image')
                 <span class="invalid-feedback" role="alert">
                     <strong>{{ $message }}</strong>
                 </span>
             @enderror
-
         </div>
+
 
         <div class="sell-form-detail-label">
             <h3 class="sell-form-detail-title">商品の詳細</h3>
         </div>
 
         <div class="sell-form-item">
-            <label for="name" class="sell-form-label">カテゴリー</label>
+            <label class="sell-form-label">カテゴリー</label>
+            <div class="category-checkbox-group">
+                @foreach([
+                    'ファッション','家電','インテリア','レディース','メンズ','コスメ',
+                    '本','ゲーム','スポーツ','キッチン','ハンドメイド',
+                    'アクセサリー','おもちゃ','ベビー・キッズ'
+                ] as $category)
+                    <input type="checkbox" name="categories[]" id="cat-{{ $loop->index }}" value="{{ $category }}" class="category-checkbox">
+                    <label for="cat-{{ $loop->index }}" class="category-label">{{ $category }}</label>
+                @endforeach
+            </div>
         </div>
 
+
         <div class="sell-form-item">
-            <label for="postal_code" class="sell-form-label">商品の状態</label>
-            <input id="postal_code" type="text" class="sell-form-input-2 @error('postal_code') is-invalid @enderror" name="postal_code" value="{{ old('postal_code') }}" required autocomplete="postal_code">
+            <label for="condition" class="sell-form-label">商品の状態</label>
+            <select id="condition" name="condition" class="sell-form-input-2 @error('condition') is-invalid @enderror" required>
+                <option value="">選択してください</option>
+                <option value="1" {{ old('condition') == 1 ? 'selected' : '' }}>良好</option>
+                <option value="2" {{ old('condition') == 2 ? 'selected' : '' }}>目立った傷や汚れなし</option>
+                <option value="3" {{ old('condition') == 3 ? 'selected' : '' }}>やや傷や汚れあり</option>
+                <option value="4" {{ old('condition') == 4 ? 'selected' : '' }}>状態が悪い</option>
+            </select>
         </div>
+
 
 
         <div class="sell-form-detail-label">
@@ -83,7 +101,7 @@
 
         <div class="sell-form-item">
             <label for="price" class="sell-form-label">販売価格</label>
-            <input id="price" type="text" class="sell-form-input-3 @error('price') is-invalid @enderror" name="price" value="{{ old('price') }}" required autocomplete="price">
+            <input id="price" type="text" class="sell-form-input-3 @error('price') is-invalid @enderror" name="price" value="{{ old('price') }}" required autocomplete="price" placeholder="¥">
             @error('price')
                 <span class="invalid-feedback" role="alert">
                     <strong>{{ $message }}</strong>
