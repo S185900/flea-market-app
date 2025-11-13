d358a38fec3c19b1e54d2c5117e647a751704812
S <serena.sa.em.na.ra@gmail.com>
2025-11-14 00:43:32 +0900

    Factory日本語設定

--- src/database/factories/UserFactory.php
+++ src/database/factories/UserFactory.php
@@ -4,6 +4,7 @@ namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
+use Faker\Factory as FakerFactory;
 
 class UserFactory extends Factory
 {
@@ -14,14 +15,16 @@ class UserFactory extends Factory
      */
     public function definition()
     {
+
         return [
-            'name' => $this->faker->name(),
-            'email' => $this->faker->unique()->safeEmail(),
-            'password' => bcrypt('password'),
+            // 'name' => $this->faker->realText(20), // ユーザー名：20文字以内
+            'name' => Str::limit($this->faker->name(), 20),
+            'email' => $this->faker->unique()->safeEmail(), // メール形式
+            'password' => bcrypt('password'), // 8文字以上
             'profile_completed' => $this->faker->boolean(),
-            'profile_image_url' => $this->faker->imageUrl(),
-            'shipping_address' => $this->faker->address(),
-            'postal_code' => $this->faker->postcode(),
+            'profile_image_url' => $this->faker->imageUrl(640, 480, 'people', true, 'profile'), // 拡張子は後で調整可能
+            'shipping_address' => $this->faker->streetAddress(), // 住所：入力必須
+            'postal_code' => $this->faker->regexify('[0-9]{3}-[0-9]{4}'), // 郵便番号：ハイフンあり8文字
             'building_name' => $this->faker->word(),
             'email_verified_at' => now(),
             'remember_token' => Str::random(10),
