6d781ac024fd6de8790edb4a3ee9b625f711c582
S <serena.sa.em.na.ra@gmail.com>
2025-11-19 22:30:25 +0900

    address_edit.blade,cssのコード品質チェック

--- src/resources/views/address_edit.blade.php
+++ src/resources/views/address_edit.blade.php
@@ -8,12 +8,14 @@
 <!-- 住所変更ページ -->
 @section('content')
 <h2 class="address-edit-title">住所の変更</h2>
-<div class="address-edit">
+<section class="address-edit">
     <form class="address-edit-form" method="POST" action="{{ route('address.update', ['item_id' => $item->id]) }}" novalidate>
         @csrf
 
         <div class="address-edit-item">
-            <label for="postal_code" class="address-edit-label">郵便番号</label>
+            <label for="postal_code" class="address-edit-label">
+                郵便番号
+            </label>
             <input id="postal_code" type="text" class="address-edit-input @error('postal_code') is-invalid @enderror"
                 name="postal_code" value="{{ old('postal_code', $address->postal_code) }}" required autocomplete="postal_code">
 
@@ -25,25 +27,33 @@
         </div>
 
         <div class="address-edit-item">
-            <label for="address" class="address-edit-label">住所</label>
+            <label for="address" class="address-edit-label">
+                住所
+            </label>
             <input id="address" type="text" class="address-edit-input @error('address') is-invalid @enderror"
                 name="address" value="{{ old('address', $address->shipping_address) }}" required autocomplete="address">
 
             @error('address')
                 <span class="invalid-feedback" role="alert">
-                    <strong class="error-message">{{ $message }}</strong>
+                    <strong class="error-message">
+                        {{ $message }}
+                    </strong>
                 </span>
             @enderror
         </div>
 
         <div class="address-edit-item">
-            <label for="building_name" class="address-edit-label">建物名</label>
+            <label for="building_name" class="address-edit-label">
+                建物名
+            </label>
             <input id="building_name" type="text" class="address-edit-input @error('building_name') is-invalid @enderror"
                 name="building_name" value="{{ old('building_name', $address->building_name) }}" autocomplete="building_name">
 
             @error('building_name')
                 <span class="invalid-feedback" role="alert">
-                    <strong class="error-message">{{ $message }}</strong>
+                    <strong class="error-message">
+                        {{ $message }}
+                    </strong>
                 </span>
             @enderror
         </div>
@@ -55,6 +65,5 @@
         </div>
     </form>
 
-
-</div>
+</section>
 @endsection
