e61a0baeddd32125e7dde2978a90db28bdba9a6e
S <serena.sa.em.na.ra@gmail.com>
2025-11-19 21:11:50 +0900

    profile_index.blade,cssのコード品質チェック

--- src/resources/views/profile_index.blade.php
+++ src/resources/views/profile_index.blade.php
@@ -25,34 +25,33 @@
 
 <div class="profile-index">
 
-    <!-- タブ切り替え -->
     <div class="items-tabs">
-        <!-- <a href="{{ route('mypage.index', ['page' => 'sell']) }}" class="items-tab {{ request('page') === 'sell' || request('page') === null ? 'active' : '' }}">出品した商品</a>
-        <a href="{{ route('mypage.index', ['page' => 'buy']) }}" class="items-tab {{ request('page') === 'buy' ? 'active' : '' }}">購入した商品</a> -->
-
         <a href="{{ route('mypage.index', ['page' => 'sell']) }}" class="items-tab {{ $page === 'sell' || $page === null ? 'active' : '' }}">出品した商品</a>
         <a href="{{ route('mypage.index', ['page' => 'buy']) }}" class="items-tab {{ $page === 'buy' ? 'active' : '' }}">購入した商品</a>
-
     </div>
 
     <div class="items-divider">
         @if ($page === 'buy')
+
             <!-- 購入商品 -->
             <div id="purchased-items" class="items-index">
                 <div class="items-grid">
+
                     @forelse ($purchasedItems as $item)
                         <a href="{{ route('item.detail', ['item_id' => $item->id]) }}" class="item-card-link">
                             <div class="item-card">
                                 <div class="item-image">
+
                                     @if ($item->images->isNotEmpty())
                                         <img class="item-image__display" src="{{ asset('storage/' . $item->images->first()->image_path) }}" alt="{{ $item->title }}">
                                     @else
                                         <div class="item-image__fallback">商品画像</div>
                                     @endif
-                                    <!-- sold表示は要件にないため非表示 -->
+
                                     @if ($item->status === 'sold' && $page !== 'buy')
                                         <div class="sold-label">sold</div>
                                     @endif
+
                                 </div>
                                 <div class="item-name">{{ strip_tags($item->title) }}</div>
                             </div>
@@ -60,24 +59,31 @@
                     @empty
                         <p>購入した商品はありません。</p>
                     @endforelse
+
                 </div>
             </div>
+
         @else
+
             <!-- 出品商品 -->
             <div id="listed-items" class="items-index">
                 <div class="items-grid">
+
                     @forelse ($listedItems as $item)
                         <a href="{{ route('item.detail', ['item_id' => $item->id]) }}" class="item-card-link">
                             <div class="item-card">
                                 <div class="item-image">
+
                                     @if ($item->images->isNotEmpty())
                                         <img class="item-image__display" src="{{ asset('storage/' . $item->images->first()->image_path) }}" alt="{{ $item->title }}">
                                     @else
                                         <div class="item-image__fallback">商品画像</div>
                                     @endif
+
                                     @if ($item->status === 'sold')
                                         <div class="sold-label">Sold</div>
                                     @endif
+
                                 </div>
                                 <div class="item-name">{{ strip_tags($item->title) }}</div>
                             </div>
@@ -85,8 +91,10 @@
                     @empty
                         <p>出品した商品はありません。</p>
                     @endforelse
+
                 </div>
             </div>
+
         @endif
     </div>
 </div>
